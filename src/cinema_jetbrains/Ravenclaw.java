4fc998d98a302988a07c558ab58b7cdc573b0df4
Keny Pau Vas <keny_pauccarima@usmp.pe>
2022-12-15 16:02:21 -0500

    ...

--- src/cinema_jetbrains/Ravenclaw.java
+++ src/cinema_jetbrains/Ravenclaw.java
@@ -1,26 +1,19 @@
 package cinema_jetbrains;
 
-import java.util.Arrays;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Ravenclaw {
 
-	// write a method here
-	public static void addValueByIndex(long[] array, int index, long value) {
-
-		array[index] += value;
-	}
-
-	// don't change the code below
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		long[] array = Arrays.stream(scanner.nextLine().split(" ")).mapToLong(Long::parseLong).toArray();
-		int index = scanner.nextInt();
-		long value = scanner.nextLong();
-		addValueByIndex(array, index, value);
-		Arrays.stream(array).forEach(e -> System.out.print(e + " "));
-		
+
+		String text = scanner.nextLine();
+
+		int number1 = scanner.nextInt();
+
+		int number2 = scanner.nextInt();
+
+		System.out.println(text.substring(number1, number2 + 1));
 		scanner.close();
 	}
 }
