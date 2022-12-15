fe0c0c25ea1e3474259b88fe4a004896b9682529
Keny Pau Vas <keny_pauccarima@usmp.pe>
2022-12-15 16:55:47 -0500

    ...

--- src/cinema_jetbrains/Ravenclaw.java
+++ src/cinema_jetbrains/Ravenclaw.java
@@ -4,22 +4,9 @@ import java.util.Scanner;
 
 public class Ravenclaw {
 
-	public static void main(String[] args) {
-		final Scanner scanner = new Scanner(System.in);
-		
-		
-		
-		/*
-
-		String text = scanner.nextLine();
-
-		int number1 = scanner.nextInt();
-
-		int number2 = scanner.nextInt();
-
-		System.out.println(text.substring(number1, number2 + 1));
-		
-		*/
-		scanner.close();
-	}
+	 public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        int age = scanner.nextInt();        
+	    	System.out.println("My age is "+ age);
+	    }
 }
