b934dd3a56365677d67e5fcb700837d32bcc0dde
Keny Pau Vas <keny_pauccarima@usmp.pe>
2022-12-15 20:13:59 -0500

    ...

--- src/cinema_jetbrains/Ravenclaw.java
+++ src/cinema_jetbrains/Ravenclaw.java
@@ -4,9 +4,27 @@ import java.util.Scanner;
 
 public class Ravenclaw {
 
-	 public static void main(String[] args) {
-	        Scanner scanner = new Scanner(System.in);
-	        int age = scanner.nextInt();        
-	    	System.out.println("My age is "+ age);
-	    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        
+        
+        try {
+        	
+        	int result = a / ((b + c) / d);
+
+            System.out.println(result);
+			
+		} catch (ArithmeticException e) {
+			System.out.println("Division by zero!");
+		}
+
+        
+        
+        scanner.close();
+    }
 }
