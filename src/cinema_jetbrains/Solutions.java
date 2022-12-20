57ff8e61c77a4e2509bec9ebce7f971ef77f9620
Keny Pau Vas <keny_pauccarima@usmp.pe>
2022-12-20 18:04:33 -0500

    ...

--- src/cinema_jetbrains/Solutions.java
+++ src/cinema_jetbrains/Solutions.java
@@ -1,8 +1,25 @@
 package cinema_jetbrains;
 
+import java.util.Scanner;
+
 public class Solutions {
 
 	 public void printIsNull(Object o) {
-	        // write your code here
-	   }
+	     if(o == null) {
+	    	 System.out.println("It's null");
+	     }else {
+	    	 System.out.println("It's an object");
+	     }
+	 }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Solutions solution = new Solutions();
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            Object obj = "null".equals(scanner.nextLine()) ? null : new Object();
+            solution.printIsNull(obj);
+        }
+    }
 }
