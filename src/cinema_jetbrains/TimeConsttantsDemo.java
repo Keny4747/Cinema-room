542dd90a208d6970ac6722ac962476730efa2a74
Keny Pau Vas <keny_pauccarima@usmp.pe>
2022-12-21 09:57:09 -0500

    ...

--- src/cinema_jetbrains/TimeConsttantsDemo.java
+++ src/cinema_jetbrains/TimeConsttantsDemo.java
@@ -1,5 +1,21 @@
 package cinema_jetbrains;
 
 public class TimeConsttantsDemo {
+	
+	public static void main(String[] args) {
+        
+        System.out.println(TimeConstants.SECONDS_IN_DAY);
+        System.out.println(TimeConstants.SECONDS_IN_WEEK);
+    }
+}
 
+final class TimeConstants {
+    public static final int SECOND = 1;
+    public static final int SECONDS_IN_MINUTE = 60 * SECOND;
+    public static final int SECONDS_IN_HOUR = 60 * SECONDS_IN_MINUTE;
+    public static final int SECONDS_IN_DAY = 24 * SECONDS_IN_HOUR;
+    public static final int SECONDS_IN_WEEK = 7 * SECONDS_IN_DAY;
+    
+
+    private TimeConstants() { }
 }
